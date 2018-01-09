13f16f44e0c97f68064511e9f3577e2517d535eb
creyesj <creyesj@DGIIARGTM03>
2018-01-09 09:23:02 -0400

    Entidad EntityBase Actualizada

--- Domain/Entities/EntityBase.cs
+++ Domain/Entities/EntityBase.cs
@@ -9,5 +9,10 @@ namespace Domain.Entities
     public class EntityBase
     {
         public int Id { get; set; }
+        public char Status { get; set; }
+        public string Registered_By { get; set; }
+        public DateTime Registered_In { get; set; }
+        public string Modified_By { get; set; }
+        public DateTime Modified_In { get; set; }
     }
 }
